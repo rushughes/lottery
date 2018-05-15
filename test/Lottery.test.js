61554cb38a270f0ff0d72ec32e4dae992174f4cc
Rus Hughes <russell.hughes@gmail.com>
2018-05-15 20:53:43 +0700

    Section 3 Lecture 80

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -1,7 +1,8 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 
 const { interface, bytecode } = require('../compile');
 
@@ -14,4 +15,26 @@ beforeEach(async () => {
   lottery = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode })
     .send({ from: accounts[0], gas: '1000000' });
+
+    lottery.setProvider(provider);
+});
+
+describe('Lottery Contract', () => {
+  it('deploys a contract', () => {
+      assert.ok(lottery.options.address);
+  });
+
+  it('allows one account to enter', async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei('0.02', 'ether')
+    });
+
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[0]
+    });
+
+    assert.equal(accounts[0], players[0]);
+    assert.equal(1, players.length);
+  });
 });
