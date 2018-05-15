3c4728c08223a383a4adefaba63710ee1ce3db52
Rus Hughes <russell.hughes@gmail.com>
2018-05-15 21:09:32 +0700

    Section 3 Lecture 83

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -25,8 +25,32 @@ describe('Lottery Contract', () => {
   });
 
   it('allows one account to enter', async () => {
+   await lottery.methods.enter().send({
+     from: accounts[0],
+     value: web3.utils.toWei('0.02', 'ether')
+   });
+
+   const players = await lottery.methods.getPlayers().call({
+     from: accounts[0]
+   });
+
+   assert.equal(accounts[0], players[0]);
+   assert.equal(1, players.length);
+ });
+
+  it('allows multiple accounts to enter', async () => {
     await lottery.methods.enter().send({
       from: accounts[0],
+      value: web3.utils.toWei('0.02', 'ether')
+    });
+
+    await lottery.methods.enter().send({
+      from: accounts[1],
+      value: web3.utils.toWei('0.02', 'ether')
+    });
+
+    await lottery.methods.enter().send({
+      from: accounts[2],
       value: web3.utils.toWei('0.02', 'ether')
     });
 
@@ -35,6 +59,33 @@ describe('Lottery Contract', () => {
     });
 
     assert.equal(accounts[0], players[0]);
-    assert.equal(1, players.length);
+    assert.equal(accounts[1], players[1]);
+    assert.equal(accounts[2], players[2]);
+    assert.equal(3, players.length);
   });
+
+  it('requires a minimum amount of ether to enter', async () => {
+    try {
+      await lottery.methods.enter().send({
+        from: accounts[0],
+        value: 200
+      });
+      assert(false);
+    } catch (err) {
+      assert(err);
+    }
+  });
+
+  it('only manager can call pickWinner', async () => {
+    try {
+      await lottery.methods.pickWinner().send({
+        from: accounts[1]
+      });
+      assert(false);
+    } catch (err) {
+      assert(err);
+    }
+
+  })
+
 });
